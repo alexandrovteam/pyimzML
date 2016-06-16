89147cf068817f1773708fc802d2474f0a130d87
Jonathan Brauer <jon.brauer@gmail.com>
2016-06-16 14:30:20 -0700

    bugfixes and comments

--- pyimzml/ImzMLWriter.py
+++ pyimzml/ImzMLWriter.py
@@ -1,17 +1,17 @@
-from lxml.objectify import ElementMaker
-import lxml.etree
+
 import os
 import numpy as np
 import uuid
 import hashlib
 import sys, getopt
+import struct
 
 from wheezy.template.engine import Engine
 from wheezy.template.ext.core import CoreExtension
 from wheezy.template.loader import DictLoader
 
 IMZML_TEMPLATE = """\
-@require(uuid, sha1sum, mz_data_type, int_data_type, run_id, spectra)
+@require(uuid, sha1sum, mz_data_type, int_data_type, run_id, spectra, mode, obo_codes)
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <mzML xmlns="http://psi.hupo.org/ms/mzml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://psi.hupo.org/ms/mzml http://psidev.info/files/ms/mzML/xsd/mzML1.1.0_idx.xsd" version="1.1">
   <cvList count="2">
@@ -22,7 +22,11 @@ IMZML_TEMPLATE = """\
   <fileDescription>
     <fileContent>
       <cvParam cvRef="MS" accession="MS:1000579" name="MS1 spectrum" value=""/>
+      @if mode == "processed":
       <cvParam cvRef="IMS" accession="IMS:1000031" name="processed" value=""/>
+      @else:
+      <cvParam cvRef="IMS" accession="IMS:1000030" name="continuous" value=""/>
+      @end
       <cvParam cvRef="IMS" accession="IMS:1000080" name="universally unique identifier" value="@uuid"/>
       <cvParam cvRef="IMS" accession="IMS:1000091" name="ibd SHA-1" value="@sha1sum"/>
     </fileContent>
@@ -31,21 +35,22 @@ IMZML_TEMPLATE = """\
   <referenceableParamGroupList count="2">
     <referenceableParamGroup id="mzArray">
       <cvParam cvRef="MS" accession="MS:1000514" name="m/z array" value=""/>
-      <cvParam cvRef="MS" accession="MS:1000523" name="@mz_data_type" value=""/>
+      <cvParam cvRef="MS" accession="MS:@obo_codes[mz_data_type]" name="@mz_data_type" value=""/>
       <cvParam cvRef="MS" accession="MS:1000576" name="no compression" value=""/>
     </referenceableParamGroup>
     <referenceableParamGroup id="intensityArray">
       <cvParam cvRef="MS" accession="MS:1000515" name="intensity array" value=""/>
-      <cvParam cvRef="MS" accession="MS:1000523" name="@int_data_type" value=""/>
+      <cvParam cvRef="MS" accession="MS:@obo_codes[int_data_type]" name="@int_data_type" value=""/>
       <cvParam cvRef="MS" accession="MS:1000576" name="no compression" value=""/>
     </referenceableParamGroup>
   </referenceableParamGroupList>
 
   <softwareList count="1">
     <software id="pyimzml" version="0.0001">
-      <cvParam cvRef="MS" accession="MS:xxx" name="pyimzml" value="pyimzml exporter"/>
+      <cvParam cvRef="MS" accession="MS:1000799" name="custom unreleased software tool" value="pyimzml exporter"/>
     </software>
   </softwareList>
+  
   <scanSettingsList count="1">
     <scanSettings id="scanSettings1">
       <cvParam cvRef="IMS" accession="IMS:1000042" name="max count of pixels x" value="@{(max(s.coords[0] for s in spectra))!!s}"/>
@@ -106,13 +111,14 @@ IMZML_TEMPLATE = """\
 
 class ImzMLWriter(object):
     def __init__(self, output_filename, mz_dtype=np.float64, intensity_dtype=np.float32):
+        '''"output_filename" is the base name, two files will be made by adding ".ibd" and ".imzML" to the basename'''
         self.mz_dtype = mz_dtype
         self.intensity_dtype = intensity_dtype
         self.run_id = os.path.splitext(output_filename)[0]
         self.filename =  self.run_id + ".imzML"
         self.ibd_filename = self.run_id + ".ibd"
         self.xml = open(self.filename, 'w')
-        self.ibd = open(self.ibd_filename, 'w')
+        self.ibd = open(self.ibd_filename, 'wb')
         self.sha1 = hashlib.sha1()
         self.ibd_offset = 0
         self.uuid = uuid.uuid4()
@@ -123,6 +129,7 @@ class ImzMLWriter(object):
         self.imzml_template = self.wheezy_engine.get_template('imzml')
 
         self.spectra = []
+        self.hashes = {} #set this to None to force processed mode.
 
         from collections import namedtuple
         self.Spectrum = namedtuple('Spectrum', ['coords', 'mz_len', 'mz_offset', 'mz_enc_len', 'int_len', 'int_offset', 'int_enc_len'])
@@ -131,26 +138,35 @@ class ImzMLWriter(object):
         spectra = self.spectra
         mz_data_type = "%d-bit float" % (np.dtype(self.mz_dtype).itemsize * 8)
         int_data_type = "%d-bit float" % (np.dtype(self.intensity_dtype).itemsize * 8)
-        uuid = '{' + self.uuid.urn.split(':')[-1].upper() + '}'
+        obo_codes = {"16-bit float": "1000520",
+            "32-bit integer": "1000519", "32-bit float": "1000521",
+            "64-bit integer": "1000522", "64-bit float": "1000523"}
+        uuid = ("{%s}"%self.uuid).upper()
         sha1sum = self.sha1.hexdigest().upper()
         run_id = self.run_id
+        mode = "processed" if self.hashes is None or len(self.hashes) != 1 else "continuous"
         self.xml.write(self.imzml_template.render(locals()))
 
+    def _encode_and_write(self, data, dtype=np.float32):
+        data = np.asarray(data, dtype=dtype)
+        offset = self.ibd_offset
+        return offset, data.shape[0], self._write_ibd(data.tobytes())
+            
     def addSpectrum(self, mzs, intensities, coords):
-		'''"mzs" and "intensities" are list-like and the same length (parallel arrays)
-		"coords" is a 2-tuple of x and y position OR a 3-tuple of x, y, and z position'''
-        mzs, intensities = np.asarray(mzs, dtype=self.mz_dtype), np.asarray(intensities, dtype=self.intensity_dtype)
-
-        mz_offset = self.ibd_offset
-        mz_len = mzs.shape[0]
-        mzs_bytes = mzs.tobytes()
-        self._write_ibd(mzs_bytes)
-        int_offset = self.ibd_offset
-        mz_enc_len = int_offset - mz_offset
-        int_len = intensities.shape[0]
-        intensities_bytes = intensities.tobytes()
-        self._write_ibd(intensities_bytes)
-        int_enc_len = self.ibd_offset - int_offset
+        '''"mzs" and "intensities" are list-like and the same length (parallel arrays)
+        "coords" is a 2-tuple of x and y position OR a 3-tuple of x, y, and z position
+        note some applications want coords to be 1-indexed'''
+        if self.hashes is not None:
+            mz_hash = hash(mzs)
+            if mz_hash in self.hashes:
+                mz_offset, mz_len, mz_enc_len = self.hashes[mz_hash]
+            else:
+                mz_offset, mz_len, mz_enc_len = self._encode_and_write(mzs, dtype=self.mz_dtype)
+                self.hashes[mz_hash] = mz_offset, mz_len, mz_enc_len
+        else:
+            mz_offset, mz_len, mz_enc_len = self._encode_and_write(mzs, dtype=self.mz_dtype)
+            
+        int_offset, int_len, int_enc_len = self._encode_and_write(intensities, self.intensity_dtype)
 
         s = self.Spectrum(mz_offset=mz_offset, mz_len=mz_len,
                           int_offset=int_offset, int_len=int_len, coords=coords,
@@ -161,12 +177,15 @@ class ImzMLWriter(object):
     def _write_ibd(self, bytes):
         self.ibd.write(bytes)
         self.sha1.update(bytes)
-        self.ibd_offset += len(bytes)
+        enc_length = len(bytes)
+        self.ibd_offset += enc_length
+        return enc_length
 
     def finish(self):
         self.ibd.close()
         self._write_xml()
         self.xml.close()
+        print "finished"
 
     def __enter__(self):
         return self
