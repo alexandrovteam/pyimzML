101253751edbe9a859a7c4558c6b7439ecc67b5e
andy-d-palmer <palmer@embl.de>
2017-01-18 14:51:56 +0100

    update test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,10 +4,13 @@ import pyimzml.ImzMLParser as imzmlp
 
 class ImzMLParser(unittest.TestCase):
     def test_bisect(self):
-        mzs, intensities = getspectrum(200, 1000, 1000)
-        test_mz = 321.0
+        mzs = [100., 201.89, 201.99, 202.0, 202.01, 202.10000001, 400.]
+        test_mz = 202.0
         test_tol = 0.1
         ix_l, ix_u = imzmlp._bisect_spectrum(mzs, test_mz, test_tol)
+        assert ix_l == 2
+        assert ix_u == 4
+        assert ix_l <= ix_u
         assert mzs[ix_l] >= test_mz - test_tol
         assert mzs[ix_u] <= test_mz + test_tol
 
